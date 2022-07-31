a8bbd93a22ca4bf5c5fe6af54b0b3a245ac0f288
chanpi23 <chandrikad34@gmail.com>
2022-07-31 16:58:36 -0400

    '#modified: src/portfolio.js'

--- src/portfolio.js
+++ src/portfolio.js
@@ -6,11 +6,11 @@ const header = {
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
+  name: 'Chandrika Dawkins',
   role: 'Front End Engineer',
   description:
     'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+  resume: 'https://docs.google.com/document/d/e/2PACX-1vSS7wXhXkv42UPXC2aWF5E2qa3owA4PAtdwWx1vRwvWoIQsLPIDvGgYXFXl1ZRz5Oolk_CBbRIx5utl/pub',
   social: {
     linkedin: 'https://linkedin.com',
     github: 'https://github.com',
